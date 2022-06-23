6fade0f6db9486e2c31d2f54d7083656b0fd92db
Zixin Zhang <zixinzha@seas.upenn.edu>
2022-06-23 16:56:08 -0400

    Remove Win32, Add x64

--- Common/DDSTextureLoader.cpp
+++ Common/DDSTextureLoader.cpp
@@ -14,7 +14,7 @@
 // http://go.microsoft.com/fwlink/?LinkId=248929
 //--------------------------------------------------------------------------------------
 
-#include "DDSTextureLoader11.h"
+#include "DDSTextureLoader.h"
 
 #include <algorithm>
 #include <cassert>
